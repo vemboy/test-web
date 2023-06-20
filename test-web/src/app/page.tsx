6f9e731c8f8953e8361fcd874fdbe530f7ca6a06
Vem Papa <vem@papazian.info>
2023-06-19 20:10:05 -0400

    Update page.tsx

--- test-web/src/app/page.tsx
+++ test-web/src/app/page.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default async function Home() {
-  const response = await axios.get("http://localhost:3001/products");
+  const response = await axios.get("http://3.15.27.30:3001/products");
   console.log(response.data);
   return response.data.map(
     (product: { id: number; title: string; desc: string }) => {
