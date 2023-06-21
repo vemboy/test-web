f9817839b8d20abba0248ccc5d9a2dabfe53e493
Vem <vem@papazian.info>
2023-06-21 03:29:26 -0400

    get rekt

--- test-web/pages/product/[id].tsx
+++ test-web/pages/product/[id].tsx
@@ -1,6 +1,11 @@
 import { useRouter } from "next/router";
-import axios from "axios";
+import axios, {AxiosError} from "axios";
 import { useEffect, useState } from "react";
+
+interface ValidationError {
+  message: string;
+  errors: Record<string, string[]>
+}
 
 export default function Page() {
   const router = useRouter();
@@ -17,6 +22,26 @@ export default function Page() {
       console.log(product);
       setProduct(product);
     });
+
+  axios.get(`http://3.15.27.30:3001/products/3`)
+  .catch(function (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log('Error', error.message);
+    }
+    console.log(error.config);
+  });
   });
 
   return (
